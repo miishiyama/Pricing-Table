cf61fbd8925af2612efc9dce54909ebbefb8723e
Millene Ishiyama <milleneeishiyama@gmail.com>
2022-12-22 14:41:25 -0300

    Fixing function

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ let Basico = document.getElementById("buttonBasico");
 let Profissional = document.getElementById("buttonProfissional");
 
 Free.onclick = function() {
-    Free.classList.toggle("blue");
+    Free.classList.add("blue");
     Basico.classList.add("white");
     Profissional.classList.remove("blue");
 }
 
 Basico.onclick = function() {
     Free.classList.remove("blue");
-    Basico.classList.toggle("white");
+    Basico.classList.remove("white");
     Profissional.classList.remove("blue");
 }
 
 Profissional.onclick = function() {
     Free.classList.remove("blue");
     Basico.classList.add("white");
-    Profissional.classList.toggle("blue")
+    Profissional.classList.add("blue")
 }
