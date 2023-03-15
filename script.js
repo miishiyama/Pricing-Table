05fd0a22483332ba2b6f3ee2414ebc61a5790626
Millene Ishiyama <milleneeishiyama@gmail.com>
2023-03-15 08:18:47 -0700

    Organizing code

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let Basico = document.getElementById("buttonBasico")
 
 let Profissional = document.getElementById("buttonProfissional")
 
-Free.onclick = function() {
+Free.onclick = function () {
     Free.classList.add("blue")
     Basico.classList.add("white")
     Profissional.classList.remove("blue")
 }
 
-Basico.onclick = function() {
+Basico.onclick = function () {
     Free.classList.remove("blue")
     Basico.classList.remove("white")
     Profissional.classList.remove("blue")
 }
 
-Profissional.onclick = function() {
+Profissional.onclick = function () {
     Free.classList.remove("blue")
     Basico.classList.add("white")
     Profissional.classList.add("blue")
